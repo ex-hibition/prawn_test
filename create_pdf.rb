55671ba352c2d64bde58801ab7c9482a116a77a1
ex-hibition <nozomumumu@gmail.com>
2017-12-04 01:14:05 +0900

    table対応

--- create_pdf.rb
+++ create_pdf.rb
@@ -25,24 +25,32 @@ class CreatePDF < Prawn::Document
 
 	# -- 住所出力
 	def renderAddress(ary: , idx:)
-		bounding_box([@x_pos, @y_pos], :width => 300) do
-			stroke_bounds
-			text "id : #{ary[0]}"
-			text "name : #{ary[1]}"
-			text "address : #{ary[2]}"
-			text "tel : #{ary[3]}"
+		bounding_box([@x_pos, @y_pos], :width => 500) do
+			text "■お客様情報"
+			move_down 5
+			data = [["会員ID","#{ary[0]}"],
+					["氏名","#{ary[1]}"],
+					["住所","#{ary[2]}"],
+					["電話番号","#{ary[3]}"]]
+			table data, :width => 500,
+						:cell_style => {:background_color => "CEECF5", :border_color => "FFFFFF"},
+						:column_widths => {0 => 150}
 		end
 
 		render_file "order_#{idx}.pdf"
-		@y_pos -= 100
+		@y_pos -= 150
 	end
 
 	# -- 注文出力
 	def renderOrder(ary:, idx:)
-		bounding_box([@x_pos, @y_pos], :width => 300) do
-			stroke_bounds
-			text "order : #{ary[4]}"
-			text "campaign : #{ary[5]}"
+		bounding_box([@x_pos, @y_pos], :width => 500) do
+			text "■お申し込み内容"
+			move_down 5
+			data = [["お申し込み商品","#{ary[4]}"],
+					["キャンペーン", "#{ary[5]}"]]
+			table data, :width => 500,
+						:cell_style => {:background_color => "CEECF5", :border_color => "FFFFFF"},
+						:column_widths => {0 => 150}
 		end
 
 		render_file "order_#{idx}.pdf"
