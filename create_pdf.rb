24d5997a884fb2d85c971e3a5f3f5da6684a47cb
ex-hibition <nozomumumu@gmail.com>
2018-03-08 02:18:00 +0900

    インデント修正

--- create_pdf.rb
+++ create_pdf.rb
@@ -6,7 +6,7 @@ Bundler.require
 
 # -- PDF作成
 class CreatePDF < Prawn::Document
-	attr_accessor :order, :x_pos, :y_pos
+  attr_accessor :order, :x_pos, :y_pos
 
   def initialize
     super()
@@ -17,7 +17,7 @@ class CreatePDF < Prawn::Document
 
   # -- ヘッダ出力
   def header
-  	# -- xy軸を描画
+    # -- xy軸を描画
     stroke_axis
     now = Time.now
     bounding_box([x_pos, y_pos], :width => 300) do
@@ -34,9 +34,9 @@ class CreatePDF < Prawn::Document
       text "■お客様情報"
       move_down 5
       data = [["会員ID","#{order[:id]}"],
-  		        ["氏名","#{order[:name]}"],
-  		        ["住所","#{order[:address]}"],
-  		        ["電話番号","#{order[:tel]}"]]
+              ["氏名","#{order[:name]}"],
+              ["住所","#{order[:address]}"],
+              ["電話番号","#{order[:tel]}"]]
       table data, :width => 500,
             :cell_style => {:background_color => "CEECF5", :border_color => "FFFFFF"},
             :column_widths => {0 => 150}
@@ -52,7 +52,7 @@ class CreatePDF < Prawn::Document
       text "■お申し込み内容"
       move_down 5
       data = [["お申し込み商品","#{order[:course]}"],
-    		      ["キャンペーン", "#{order[:campaign]}"]]
+              ["キャンペーン", "#{order[:campaign]}"]]
       table data, :width => 500,
             :cell_style => {:background_color => "CEECF5", :border_color => "FFFFFF"},
             :column_widths => {0 => 150}
@@ -62,31 +62,31 @@ class CreatePDF < Prawn::Document
     self.y_pos -= 100
   end
 
-	# -- 入力データ構造を定義
-	Order = Struct.new(:num, :id, :name, :address, :tel, :course, :campaign)
-	# -- csvファイルをOrderとして取り込む
+  # -- 入力データ構造を定義
+  Order = Struct.new(:num, :id, :name, :address, :tel, :course, :campaign)
+  # -- csvファイルをOrderとして取り込む
   def importOrder(line:, idx:)
-		self.order = Order.new(idx,line[0],line[1],line[2],line[3],line[4],line[5])
+    self.order = Order.new(idx,line[0],line[1],line[2],line[3],line[4],line[5])
   end
 end
 
 
 if $0 == __FILE__
-	begin
-		# -- 入力ファイルCSV
-		INPUT = ARGV[0]
-		  
-		# -- CSVファイルを1行ずつ処理
-		CSV.foreach(INPUT).with_index do |line, i|
-			pdf = CreatePDF.new
-		
-			pdf.importOrder(line: line, idx: i)
-		  pdf.header
-		  pdf.renderAddress
-		  pdf.renderOrder
-		end
-	rescue => err
-		p err
-		exit 1
-	end
+  begin
+    # -- 入力ファイルCSV
+    INPUT = ARGV[0]
+      
+    # -- CSVファイルを1行ずつ処理
+    CSV.foreach(INPUT).with_index do |line, i|
+      pdf = CreatePDF.new
+    
+      pdf.importOrder(line: line, idx: i)
+      pdf.header
+      pdf.renderAddress
+      pdf.renderOrder
+    end
+  rescue => err
+    p err
+    exit 1
+  end
 end
